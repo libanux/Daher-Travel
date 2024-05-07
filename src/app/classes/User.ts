e8224bd8199bd4fac0e016e059ce5a61788662c1
DanahAlAbdallah <danaalabdallah02@gmail.com>
2024-05-07 11:10:55 +0200

    show user details

---
+++ src/app/classes/User.ts
@@ -0,0 +1,16 @@
+export class User {
+    user_ID: number = 0;
+    owner_ID: number = 0;
+    google_U: string = '';
+    first_NAME: string = '';
+    last_NAME: string = '';
+    username: string = '';
+    email: string = '';
+    password: string = '';
+    user_TYPE_CODE: string = '';
+    user_LANG_CODE: string = '';
+    is_ACTIVE: boolean = true;
+    is_DELETED: boolean = false;
+    profile_COMPLETED: boolean = true;
+    entry_DATE: string = '';
+}
