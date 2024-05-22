5f58af06da8ed03d3e678004883ded9444ae8779
DanahAlAbdallah <danaalabdallah02@gmail.com>
2024-05-22 14:34:45 +0200

    dropdown functionality

--- src/app/shared-module/dropdown-filter/dropdown-filter.component.ts
+++ src/app/shared-module/dropdown-filter/dropdown-filter.component.ts
@@ -1,4 +1,5 @@
 import { Component, Input, SimpleChanges } from '@angular/core';
+import { DropdownService } from '../../signals/dropdown.service';
 
 @Component({
   selector: 'app-dropdown-filter',
@@ -6,35 +7,46 @@ import { Component, Input, SimpleChanges } from '@angular/core';
   styleUrls: ['./dropdown-filter.component.css']
 })
 export class DropdownFilterComponent {
-@Input() dropTitle: string =''
-@Input() dropOptions: string[] =[]
+  @Input() dropTitle: string = ''
+  @Input() dropOptions: string[] = []
 
-@Input() dropTitle1: string =''
-@Input() dropOptions1: string[] =[]
+  @Input() dropTitle1: string = ''
+  @Input() dropOptions1: string[] = []
 
-@Input() dropTitle2: string =''
-@Input() dropOptions2: string[] =[]
-
-
-selectedOption1: string='';
-selectedOption2: string='';
-selectedOption3: string='';
-
-// constructor(private drop){}
+  @Input() dropTitle2: string = ''
+  @Input() dropOptions2: string[] = []
 
 
-onDrop1Change( event: any) {
-  
+  selectedOption1: string = '';
+  selectedOption2: string = '';
+  selectedOption3: string = '';
 
-}
+  constructor(private dropService: DropdownService) { }
 
-onDrop2Change( event: any) {
-  
 
-}
+  onDrop1Change(event: any) {
+    if(event == "All"){
+      this.dropService.DropDown1.set('');
+    }else{
+      this.dropService.DropDown1.set(event);
+    }
+  }
 
-onDrop3Change( event: any) {
-  
+  onDrop2Change(event: any) {
+    if(event == "All"){
+      this.dropService.DropDown2.set('');
+    }else{
+    this.dropService.DropDown2.set(event);
 
+  }}
+
+  onDrop3Change(event: any) {
+
+    if(event == "All"){
+      this.dropService.DropDown3.set('');
+    }else{
+    this.dropService.DropDown3.set(event);
+
+  }
 }
 }
