eee7fa81ef0ab2c29739ed41bc09f127a90227f5
nancy-chaalan <150782100+nancy-chaalan@users.noreply.github.com>
2024-05-22 11:28:29 +0300

    finished paging

--- src/app/shared-module/pagination/pagination.component.ts
+++ src/app/shared-module/pagination/pagination.component.ts
@@ -5,118 +5,148 @@ import { Component, EventEmitter, Input, OnChanges, OnInit, Output, SimpleChange
   templateUrl: './pagination.component.html',
   styleUrls: ['./pagination.component.css']
 })
-export class PaginationComponent implements OnInit, OnChanges {
+export class PaginationComponent implements OnChanges , OnInit{
+  data: any [] = [];
 
-  @Input() data: any [] = [];
-  @Input() lengthArray: number = 0;
-  @Input() pageSize = 2;
-
-  @Output() dataPerPage = new EventEmitter<any[]>();
-  @Output() currentPageEvent = new EventEmitter<number>();
- 
-  maxPages: number = 3;
-  pageSizeOptions: number[] = [5, 10, 20];
   @Input() currentPage: number = 0;
-  pagedProducts: any[] = [];
+  @Input() pageSize = 0;
+  
+  @Output() currentPageEvent = new EventEmitter<number>();
 
-  ngOnChanges(changes: SimpleChanges): void {
-    if ('currentPage' in changes) {
-    this.currentPage = this.currentPage
-    this.getPageNumbers()
+constructor(){}
+
+ngOnInit(): void {
+
+// this.subjectServices.receiveAlertData().subscribe(data => {
+//     this.currentPage = 0 ;
+//     this.currentPageEvent.emit(this.currentPage);
+// });
+
+    // this.subjectServices.recievePaymentMethod().subscribe({
+    //   next: (response: any) => {this.currentPage = 0  },
+    //   error: (error) => {  },
+    //   complete: () => {    }
+    //   });
+  
+    //   this.subjectServices.recieveTransactionStatus().subscribe({
+    //     next: (response: any) => {this.currentPage = 0}, 
+    //     error: (error) => {  },
+    //     complete: () => {   }
+    //   });
+
+    //   this.subjectServices.recieveSelectedProductStatus().subscribe({
+    //     next: (response: any) => {this.currentPage = 0}, 
+    //     error: (error) => {},
+    //     complete: () => { }
+    //   });
+        
+    //   this.subjectServices.recieveIfSorting().subscribe({
+    //     next: (response: any) => {this.currentPage = 0}, 
+    //     error: (error) => {},
+    //     complete: () => { }
+    //   });
+}
+
+ngOnChanges(changes: SimpleChanges): void {
+  if ('pageSize' in changes || 'currentPage' in changes) {
+
+    console.log(this.pageSize)
+    this.generatePageArray(this.pageSize);
+
+    if(this.currentPage>0){this.disabelBackBtn = false}
+    else {this.disabelBackBtn = true}
+    
+    if(this.currentPage+1 ==this.pageSize){this.disabelNextBtn = true}
+    else {this.disabelNextBtn = false}
+
+    if(this.pageSize==0){this.disabelBackBtn = true; this.disabelNextBtn = true}
+
   }
-  }
 
-ngOnInit() {
-  // Initial emit of pagedProducts
-  this.onPageChange({ pageIndex: this.currentPage, pageSize: this.pageSize });
 }
 
-onPageChange(event: any): void {
-  this.currentPage = event.pageIndex;
-  this.currentPageEvent.emit(this.currentPage);
-
-  const startIndex = this.calculateStartIndex();
-  const endIndex = this.calculateEndIndex();
-  this.pagedProducts = this.data.slice(startIndex, endIndex);
-
-  this.dataPerPage.emit(this.pagedProducts);
-}
-
-goToPage(page: number): void {
-  this.currentPage = page;
-  this.currentPageEvent.emit(this.currentPage);
-  this.onPageChange({ pageIndex: this.currentPage, pageSize: this.pageSize });
-}
-
-goToPreviousPage(): void {
-  if (this.currentPage > 0) {
-    this.goToPage(this.currentPage - 1);
+generatePageArray(pageSize: number){
+  this.data = []; // Clear the array before populating it again
+  this.currentPage = 0;
+  for(let i = 1; i <= pageSize; i++){
+    this.data.push(i);
   }
 }
 
-goToNextPage(): void {
-  const totalPages = Math.ceil(this.lengthArray / this.pageSize);
-    if (this.currentPage < totalPages - 1) {
-      this.goToPage(this.currentPage + 1);
-    }
-}
-
-// getPageNumbers(): number[] {
-//     const totalPages = Math.ceil(this.lengthArray / this.pageSize);
-
-//     let pagesToShow: number[] = [];
-  
-//     // Always show the first two pages
-//     for (let i = 0; i < Math.min(totalPages, 2); i++) {
-//       pagesToShow.push(i);
-//     }
-  
-//     // If there are more than two pages, show an ellipsis and the last page
-//     if (totalPages > 2) {
-//       pagesToShow.push(-1); // -1 represents the ellipsis
-//       pagesToShow.push(totalPages - 1);
-//     } else {
-//       // Otherwise, show all remaining pages
-//       for (let i = 2; i < totalPages; i++) {
-//         pagesToShow.push(i);
-//       }
-//     }
-  
-//     return pagesToShow;
-//   }
-  
 getPageNumbers(): number[] {
-  const totalPages = Math.ceil(this.lengthArray / this.pageSize);
   const currentPage = this.currentPage;
 
-  if (totalPages <= 3) {
-      // If there are 3 or fewer pages, display all pages
-      return Array.from({ length: totalPages }, (_, i) => i);
-  } 
-  
-  else {
-      // If there are more than 3 pages, display first 2, then ellipsis, then last page
-      if (currentPage < 2) {
-          // If current page is one of the first 2 pages
-          return [0, 1, -1, totalPages - 1];
-      } else if (currentPage > totalPages - 3) {
-          // If current page is one of the last 2 pages
-          return [0, -1, totalPages - 3, totalPages - 2, totalPages - 1];
-      } else {
-          // If current page is in the middle pages
-          return [0, -1, currentPage - 1, currentPage, currentPage + 1, -1, totalPages - 1];
-      }
+  if (this.pageSize <= 3) {
+    // If there are 3 or fewer pages, display all pages starting from 1
+    return Array.from({ length: this.pageSize }, (_, i) => i + 1);
+  } else {
+    // If there are more than 3 pages
+    if (currentPage < 2) {
+      // If current page is one of the first 2 pages
+      return [1, 2, -1, this.pageSize];
+    } else if (currentPage > this.pageSize - 3) {
+      // If current page is one of the last 2 pages
+      return [1, -1, this.pageSize - 2, this.pageSize - 1, this.pageSize];
+    } else {
+      // If current page is in the middle pages
+      return [1, -1, currentPage, currentPage + 1, currentPage + 2, -1, this.pageSize];
+    }
   }
 }
 
-calculateStartIndex(): number {
-  return this.currentPage * this.pageSize;
+
+goToPage(page: number): void {
+  this.currentPage = page - 1;
+
+  if(this.currentPage>0){
+    this.disabelBackBtn = false
+  }
+
+  else {
+    this.disabelBackBtn = true
+  }
+
+  if(this.currentPage+1 ==this.pageSize){
+    this.disabelNextBtn = true
+  }
+
+  else {
+    this.disabelNextBtn = false
+  }
+
+  this.currentPageEvent.emit(this.currentPage);
 }
 
-calculateEndIndex(): number {
-  const end = Math.min((this.currentPage + 1) * this.pageSize, this.lengthArray);
-  return end;
+disabelBackBtn = true;
+goToPreviousPage(): void {
+this.currentPage --;
+this.disabelNextBtn = false;
+
+  if(this.currentPage == 0){
+    this.disabelBackBtn = true
+  }
+
+  else {
+    this.disabelBackBtn = false
+  }
+
+  this.currentPageEvent.emit(this.currentPage);
 }
 
+disabelNextBtn = false;
+goToNextPage(): void {
+  this.currentPage ++;
+  this.disabelBackBtn = false;
+
+  if(this.currentPage+1 ==this.pageSize){
+    this.disabelNextBtn = true
+  }
+
+  else {
+    this.disabelNextBtn = false
+  }
+
+  this.currentPageEvent.emit(this.currentPage);
+}
 
 }
--- src/app/shared-module/web-translation-table/web-translation-table.component.ts
+++ src/app/shared-module/web-translation-table/web-translation-table.component.ts
@@ -1,9 +1,10 @@
-import { Component, Input } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 
 import { UserService } from '../../service-folder/user.service';
 import { User } from '../../classes/User';
 import { Router } from '@angular/router';
 import { TranslationService } from '../../service-folder/translation.service';
+import { GeneralService } from '../../service-folder/general.service';
 
 @Component({
   selector: 'app-web-translation-table',
@@ -14,6 +15,7 @@ export class WebTranslationTableComponent {
 
   @Input() showDropdowns = true;
   @Input() showTitle = true;
+  @Input() showPaging: boolean = true ;
 
   status: string = "ACCEPTED"
 
@@ -23,10 +25,10 @@ export class WebTranslationTableComponent {
   dropOptions1: string[] = ["Option1", "Option2", "Option3"];
   dropOptions2: string[] = ["Option1", "Option2", "Option3"];
   dropOptions3: string[] = ["Option1", "Option2", "Option3"];
+  private apiUrl = '';
 
-
-  constructor(private router: Router, private translationsService: TranslationService) { }
-
+  constructor(private apiService : GeneralService, private router: Router, private translationsService: TranslationService) { }
+  webTranslationArrayLength = 0
   webTranslationArray: any[] = [];
   showShimmer: boolean = true;
   currentPage = 0;
@@ -35,16 +37,20 @@ export class WebTranslationTableComponent {
     this.translationsService.GET_WEB_TRANSLATION_PER_PAGE(this.currentPage).subscribe({
       next: (response: any) => {
         this.webTranslationArray = response.my_QUOTE_TRANSLATIONS.first;
+        this.webTranslationArrayLength = Math.ceil(response.my_QUOTE_TRANSLATIONS.second / this.apiService.PageSizing);
       },
-      error: (error: any) => { this.showShimmer = false; console.log(error); },
+      error: (error: any) => { this.showShimmer = false; },
       complete: () => { this.showShimmer = false; }
     });
   }
 
-  // //ADD USER FUNCTION
-  // addUser() {
-  //   this.router.navigate(['/users/add']);
-  // }
+  @Output() currentPageChoosen = new EventEmitter<number>();
+
+  receivePageSize($event: any) {
+    this.currentPage = $event;
+    this.currentPageChoosen.emit(this.currentPage)
+  }
+  
 
   // function for viewing a specific item
   moveToRouteWithIndex(route: string, id: number) {
@@ -52,4 +58,6 @@ export class WebTranslationTableComponent {
       window.scrollTo(0, 0);
     });
   }
+
+
 }
