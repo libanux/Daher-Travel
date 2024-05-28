48070d9a1649ed001a1d99a007f40c4b8ade269f
DanahAlAbdallah <danaalabdallah02@gmail.com>
2024-05-28 13:46:23 +0200

    chatting system

--- src/app/service-folder/chat.service.ts
+++ src/app/service-folder/chat.service.ts
@@ -1,29 +1,63 @@
 import { Injectable } from '@angular/core';
-import { WebSocketService } from './web-socket.service';
-import { Subject } from 'rxjs';
-import { map } from 'rxjs/operators';
+import SockJS from 'sockjs-client';
+import * as Stomp from 'stompjs';
+import { GeneralService } from './general.service';
 
 @Injectable({
-  providedIn: 'root',
+  providedIn: 'root'
 })
 export class ChatService {
+  public stompClient: Stomp.Client | null = null;
+  public msg: string[] = [];
+  public socket ?: WebSocket
+  token: string =''
 
-  messages: Subject<any>;
-
-  // Our constructor calls our wsService connect method
-  constructor(private wsService: WebSocketService) {
-    this.messages = <Subject<any>>wsService
-      .connect()
-      .pipe(
-        map((response: any): any => {
-          return response;
-        })
-      );
+  constructor(private general: GeneralService) {
+    this.token = this.general.storedToken
+    this.initializeWebSocketConnection();
   }
 
-  // Our simplified interface for sending
-  // messages back to our socket.io server
-  sendMsg(msg: any) {
-    this.messages.next(msg);
+  initializeWebSocketConnection() {
+    let socketUrl = `https://libanux.xyz/servsmart/websocket?Ticket=Bearer ${this.token}`;
+    this.socket = new SockJS(socketUrl);
+    this.stompClient = Stomp.over(this.socket!);
+    this.stompClient.connect({
+      Authorization: "Bearer " + this.general.storedToken
+    }, (frame:any) => {
+      console.log("connection established: " + frame);
+      this.stompClient?.subscribe('/topic/public', (item: Stomp.Message) => {
+        let notifications = JSON.parse(item.body);
+        console.log(notifications)
+      });
+      // this.stompClient?.send('/ws/chat.register', { Authorization: authToken });
+      // this.scheduleMessages();
+    })
+  }
+
+  sendMessage(message: any) {
+    const exampleParamsEditTranslationOrderFile = {
+      TRANSLATION_ORDER_FILE_ID: 12345,
+      TYPE: "type1",
+      FILE_ID: 67890,
+      TRANSLATION_ORDER_ID: 11223,
+      USER_ID: 44556,
+      COMMENT: "This is a comment.",
+      TIME_CREATION: "2024-05-28T14:00:00Z",
+      ENTRY_USER_ID: 78901,
+      ENTRY_DATE: "2024-05-28",
+      OWNER_ID: 5
+  };
+  var body = JSON.stringify(exampleParamsEditTranslationOrderFile)
+    this.stompClient?.send('/ws/chat.register', { Authorization: "Bearer " + this.token }, body);
+    if (this.stompClient && this.stompClient.connected) {
+      // this.stompClient.send('/chat.register', {}, message);
+      this.msg.push(message)
+    } else {
+      console.error('STOMP client is not connected.');
+    }
+  }
+
+  private onError(error: string | Stomp.Frame) {
+    console.error('Error with STOMP client:', error);
   }
 }
--- src/app/shared-module/chatting/chatting.component.ts
+++ src/app/shared-module/chatting/chatting.component.ts
@@ -1,22 +1,84 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { ChatService } from '../../service-folder/chat.service';
+import { TranslationService } from '../../service-folder/translation.service';
+
 
 @Component({
   selector: 'app-chatting',
   templateUrl: './chatting.component.html',
   styleUrl: './chatting.component.css'
 })
-export class ChattingComponent {
-  message: string =""
-    constructor(private chat: ChatService){ }
-  
-    ngOnInit() {
-      this.chat.messages?.subscribe(msg => {
-        console.log(msg);
-      })
+export class ChattingComponent implements OnInit {
+  input: string = '';
+  msgArray: string[] = []
+  file?: any
+  uploadedFiles: File[] = [];
+  userId: any = 0
+  fileID: number = 0;
+
+  constructor(private translationService: TranslationService, private chatService: ChatService) {
+    this.userId = localStorage.getItem('userId')
+  }
+
+  ngOnInit(): void {
+    this.msgArray = this.chatService.msg
+  }
+
+  sendMessage() {
+    this.chatService.sendMessage(this.fileID);
+    this.input = '';
+  }
+
+
+  onFileChange(event: any) {
+    const inputElement = event.target as HTMLInputElement;
+    const selectedFile = inputElement.files ? inputElement.files[0] : null;
+
+    const file: File = event.target.files[0];
+
+    const files: FileList = event.target.files;
+    this.ADD_FILE(selectedFile);
+    this.file = selectedFile
+
+    for (let i = 0; i < files.length; i++) {
+      this.uploadedFiles.push(files[i]);
     }
-  
-    sendMessage() {
-      this.chat.sendMsg(this.message);
-    }
+
+  }
+
+  onCommentChange(event: any) {
+    console.log(event)
+  }
+
+  All_Files_Array: any[] = [];
+  SAVE_CHANGES() {
+    this.translationService.EDIT_TRANSLATION_ORDER_FILE_LIST(this.All_Files_Array, this.userId).subscribe({
+      next: (response: any) => { console.log(response); },
+      error: (error: any) => { console.error(error) },
+      complete: () => {
+        // Clear the uploadedFiles array
+        this.uploadedFiles = [];
+      }
+    });
+  }
+
+  file_added: any = { id: 0, comment: '' };
+  ADD_FILE(files: any) {
+    this.translationService.EDIT_TRANSLATION_FILE(files, this.userId).subscribe({
+      next: (response: any) => {
+        this.fileID = response.my_FILE.file_ID
+        this.file_added = {
+          id: response.my_FILE.file_ID,
+          comment: response.my_FILE.comment
+        };
+        console.log(this.file_added);
+      },
+      error: (error: any) => { console.error(error) },
+
+      complete: () => {
+        this.All_Files_Array.push(this.file_added);
+        console.log('completed ', this.All_Files_Array)
+      }
+    });
+  }
 }
