f2f9509417d28ba5304536fbb488294698ad1b11
DanahAlAbdallah <danaalabdallah02@gmail.com>
2024-05-29 16:12:37 +0200

    editand proof reading

--- src/app/translation/edit-proofreadin-form/edit-proofreadin-form.component.ts
+++ src/app/translation/edit-proofreadin-form/edit-proofreadin-form.component.ts
@@ -9,49 +9,47 @@ import { ActivatedRoute } from '@angular/router';
   styleUrl: './edit-proofreadin-form.component.css'
 })
 export class EditProofreadinFormComponent {
-  selected_Translation =  signal(0);
+  selected_Translation = signal(0);
   translation_ID = 0;
 
-  FileArray : any [] =[]
-  translation_Info : any = {}
+  FileArray: any[] = []
+  translation_Info: any = {}
 
-  constructor(private translationSignal : TranslationSignalService, private translationService : TranslationService, private route: ActivatedRoute) {}
+  constructor(private translationSignal: TranslationSignalService, private translationService: TranslationService, private route: ActivatedRoute) { }
 
-ngOnInit(): void {
-this.selected_Translation = this.translationSignal.selected_Translation_ID
-this.GET_TRANSLATION_BY_ID()
-}
+  ngOnInit(): void {
+    this.selected_Translation = this.translationSignal.selected_Translation_ID
+    this.GET_TRANSLATION_BY_ID()
+  }
 
-GET_TRANSLATION_BY_ID(){
-  this.translationService.GET_TRANSLATION_BY_ID(this.selected_Translation()).subscribe({
-    next : (response: any) => {this.translation_Info = response.my_TRANSLATION_ORDER;},
-    error: (error: any) => {console.error(error)},
-    complete: () => {this.GET_FILES_ARRAY()}
-  });
-}
+  GET_TRANSLATION_BY_ID() {
+    this.translationService.GET_TRANSLATION_BY_ID(this.selected_Translation()).subscribe({
+      next: (response: any) => {
+        this.translation_Info = response.my_TRANSLATION_ORDER;
+        console.log("Response:", response)
 
-GET_FILES_ARRAY(){
-  this.translationService.GET_FILES_TRANSLATION_BY_ID(this.selected_Translation()).subscribe({
-    next : (response: any) => {
-      this.FileArray = response.my_Translation_ORDER_FILES.filter((file: any) => file.type === 'REQ');
-
-      // for(let i = 0; i < response.my_Translation_ORDER_FILES.length ; i++){
-      //   if( response.my_Translation_ORDER_FILES[i].type == 'REQ'){
-      //     this.FileArray.push( response.my_Translation_ORDER_FILES[i])
-      //   }
-      // }
       },
-    error: (error: any) => {console.error(error)},
-    complete: () => {}
-  });
-}
+      error: (error: any) => { console.error(error) },
+      complete: () => { this.GET_FILES_ARRAY() }
+    });
+  }
 
-DELETE_FILE(ID: number){
-  this.translationService.DELETE_FILE_BY_TRANSLATION_ORDER_FILE_ID(ID).subscribe({
-    next : (response: any) => {},
-    error: (error: any) => {console.error(error)},
-    complete: () => {this.GET_FILES_ARRAY()}
-  });
-}
+  GET_FILES_ARRAY() {
+    this.translationService.GET_FILES_TRANSLATION_BY_ID(this.selected_Translation()).subscribe({
+      next: (response: any) => {
+        this.FileArray = response.my_Translation_ORDER_FILES.filter((file: any) => file.type === 'REQ');
+
+        // for(let i = 0; i < response.my_Translation_ORDER_FILES.length ; i++){
+        //   if( response.my_Translation_ORDER_FILES[i].type == 'REQ'){
+        //     this.FileArray.push( response.my_Translation_ORDER_FILES[i])
+        //   }
+        // }
+      },
+      error: (error: any) => { console.error(error) },
+      complete: () => { }
+    });
+  }
+
+
 
 }
