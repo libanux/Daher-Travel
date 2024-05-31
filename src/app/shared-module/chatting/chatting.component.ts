d09c64eec5a0d3cc7bd0bb34b989792b3013dfae
nancy-chaalan <150782100+nancy-chaalan@users.noreply.github.com>
2024-05-31 10:12:11 +0300

    new changes

--- src/app/shared-module/chatting/chatting.component.ts
+++ src/app/shared-module/chatting/chatting.component.ts
@@ -27,18 +27,18 @@ export class ChattingComponent implements OnInit {
     this.userId = this.generalService.userId;
 
     effect(()=>{
-      console.log('a chat was sent ', this.NEW_ARRAY());
+      // console.log('a chat was sent ', this.NEW_ARRAY());
       this.Files_Array = this.NEW_ARRAY();
     });
   }
 
-  ngOnInit(): void {
+ngOnInit(): void {
     this.TRANSLATION_ID = this.translationSignal.selected_Translation_ID
     this.NEW_ARRAY = this.chatSignal.NEW_ARRAY
     this.GET_FILES_ARRAY();
-  }
+}
 
-  openFile(file_ID: number) {
+OPEN_FILE(file_ID: number) {
     this.translationService.GET_FILE_BY_FILE_ID(file_ID).subscribe({
       next: (response: any) => {
         console.log(response.my_File.file_URL)
@@ -49,9 +49,9 @@ export class ChattingComponent implements OnInit {
 
     });
 
-  }
+}
 
-  SEND_MESSAGE() {
+SEND_MESSAGE() {
 
     if(this.FILE_NAME != "Type your message here!")
       {
@@ -68,9 +68,9 @@ export class ChattingComponent implements OnInit {
 
     else( console.error('empty message')  )
 
-  }
+}
 
-  onFileChange(event: any) {
+onFileChange(event: any) {
     const inputElement = event.target as HTMLInputElement;
     console.log(inputElement.files)
     const selectedFile = inputElement.files ? inputElement.files[0] : null;
@@ -83,28 +83,25 @@ export class ChattingComponent implements OnInit {
     for (let i = 0; i < files.length; i++) {
       this.uploadedFiles.push(files[i]);
     }
-  }
+}
 
-  All_Files_Array: any[] = [];
-  SAVE_CHANGES() {
+All_Files_Array: any[] = [];
+SAVE_CHANGES() {
     this.All_Files_Array.push(this.file_added);
     this.translationService.EDIT_TRANSLATION_ORDER_FILE_LIST(this.All_Files_Array, this.userId, this.TRANSLATION_ID()).subscribe({
-      next: (response: any) => { },
-      error: (error: any) => { console.error(error) },
-      complete: () => {
-        // Clear the uploadedFiles array
-        // this.GET_FILES_ARRAY()
-        this.All_Files_Array = [];
-        this.message = '';
-        this.FILE_NAME = "Type your message here!";
-        this.chatService.SEND_MESSAGE(this.message, this.FILE_ID, this.userId, this.TRANSLATION_ID());
-
-      }
-    });
+  next: (response: any) => { },
+  error: (error: any) => { console.error(error) },
+  complete: () => {
+    this.All_Files_Array = [];
+    this.message = '';
+    this.FILE_NAME = "Type your message here!";
+    this.chatService.SEND_MESSAGE(this.message, this.FILE_ID, this.userId, this.TRANSLATION_ID());
   }
+    });
+}
 
-  file_added: any = { id: 0, comment: '' };
-  ADD_FILE(files: any) {
+file_added: any = { id: 0, comment: '' };
+ADD_FILE(files: any) {
     this.translationService.EDIT_TRANSLATION_FILE(files, this.userId).subscribe({
       next: (response: any) => {
         this.FILE_ID = response.my_FILE.file_ID
@@ -117,15 +114,15 @@ export class ChattingComponent implements OnInit {
       console.error(error);       
       this.FILE_NAME = "Type your message here!" },
       complete: () => {this.uploadedFiles = [];}
-    });
-  }
+  });
+}
 
-  GET_FILES_ARRAY() {
-    this.translationService.GET_FILES_TRANSLATION_BY_ID(this.TRANSLATION_ID()).subscribe({
-      next : (response: any) => {this.Files_Array = response.my_Translation_ORDER_FILES.filter((file: any) => file.type === 'RES');   },
-      error: (error) => {console.error(error)},
-      complete: () => {}
-    });
-  }
+GET_FILES_ARRAY() {
+  this.translationService.GET_FILES_TRANSLATION_BY_ID(this.TRANSLATION_ID()).subscribe({
+    next : (response: any) => {this.Files_Array = response.my_Translation_ORDER_FILES.filter((file: any) => file.type === 'RES');   },
+    error: (error) => {console.error(error)},
+    complete: () => {}
+  });
+}
   
 }
