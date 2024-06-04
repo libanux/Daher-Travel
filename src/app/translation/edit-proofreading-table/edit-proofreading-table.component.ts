4e42709b66f52c3a39f5b47e90c0cf7830b152bd
DanahAlAbdallah <danaalabdallah02@gmail.com>
2024-06-04 13:57:30 +0200

    count box designs

--- src/app/translation/edit-proofreading-table/edit-proofreading-table.component.ts
+++ src/app/translation/edit-proofreading-table/edit-proofreading-table.component.ts
@@ -10,49 +10,12 @@ import { TranslationSignalService } from '../../signals/translation-signal.servi
   styleUrl: './edit-proofreading-table.component.css'
 })
 export class EditProofreadingTableComponent {
-  @Input() showDropdowns = true;
-  @Input() showTitle = true;
-  @Input() showPaging: boolean = true ;
 
-  @Output() currentPageChoosen = new EventEmitter<number>();
-
-  selected_Translation = signal(0);
-
-  editAndProofreadingArrayLength = 0
-  editAndProofreadingArray: any[] = [];
-  showShimmer: boolean = true;
-  currentPage = 0;
-
-  constructor(private apiService : GeneralService, private router: Router, private translationsService: TranslationService,private translationSignal: TranslationSignalService,) { }
-
-  ngOnInit(): void {
-    this.selected_Translation = this.translationSignal.selected_Translation_ID;
-   this.FETCH_EDITING_AND_PROOFREADING_TRANSLATION(0);
-  }
-
-  receivePageSize($event: any) {
-    this.currentPage = $event;
-    this.currentPageChoosen.emit(this.currentPage)
-    this.FETCH_EDITING_AND_PROOFREADING_TRANSLATION(this.currentPage)
-  }
-
-  FETCH_EDITING_AND_PROOFREADING_TRANSLATION(page: number){
-    this.translationsService.GET_TRANSLATION_ORDER_BY_WHERE_IN_LIST_ADV1(page).subscribe({
-      next: (response: any) => {
-        this.editAndProofreadingArray = response.my_Translation_Orders.first;
-        this.editAndProofreadingArrayLength = Math.ceil(response.my_Translation_Orders.second / this.apiService.PageSizing);
-      },
-      error: (error: any) => { this.showShimmer = false; },
-      complete: () => { this.showShimmer = false; }
-    });
-  }
-  
-
-  // function for viewing a specific item
+  // MOVE TO SPECIFIC ROUTE WITH ROW ID 
   moveToRouteWithIndex(route: string, id: number) {
-    this.selected_Translation.set(id);
-    this.router.navigate([route], { queryParams: { id: id } }).then(() => {
-      window.scrollTo(0, 0);
-    });
+    // this.selected_Translation.set(id);
+    // this.router.navigate([route], { queryParams: { id: id } }).then(() => {
+    //   window.scrollTo(0, 0);
+    // });
   }
 }
