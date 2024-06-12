e953024da8cef9f3a59575820bed37f23dc7644b
DanahAlAbdallah <danaalabdallah02@gmail.com>
2024-06-12 12:53:10 +0200

    admin prof

--- src/enviroment/enviroment.ts
+++ src/enviroment/enviroment.ts
@@ -3,7 +3,7 @@
 // The list of file replacements can be found in `angular.json`.
 export const environment = {
     // apiLocalBaseUrl: "https://libanux.xyz/DaherTravel/api",
-    apiLocalBaseUrl: "http://192.168.1.6:5000/DaherTravel/api",
+    apiLocalBaseUrl: "http://192.168.1.11:5000/DaherTravel/api",
     ws_url: "http://localhost:5000"
   };
   /*
